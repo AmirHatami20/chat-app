c7cc10ceca48765610b084b23588460388d30602
Amir Hatami <hatamiamir055@gmail.com>
2026-02-14 21:31:49 +0330

    clean code

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,18 +1,18 @@
 import api from "../utils/axios";
-import type {LoginPayload, RegisterPayload} from "../types";
+import type { LoginPayload, RegisterPayload } from "../types";
 
 export const authService = {
-    register: async (data: RegisterPayload) => {
-        const res = await api.post("/auth/register", data);
-        return res.data;
-    },
-    login: async (data: LoginPayload) => {
-        const res = await api.post("/auth/login", data);
-        localStorage.setItem("accessToken", res.data.accessToken);
-        return res.data;
-    },
-    logout: async () => {
-        await api.post("/auth/logout");
-        localStorage.removeItem("accessToken");
-    },
-}
+  register: async (data: RegisterPayload) => {
+    const res = await api.post("/auth/register", data);
+    return res.data;
+  },
+  login: async (data: LoginPayload) => {
+    const res = await api.post("/auth/login", data);
+    localStorage.setItem("accessToken", res.data.accessToken);
+    return res.data;
+  },
+  logout: async () => {
+    await api.post("/auth/logout");
+    localStorage.removeItem("accessToken");
+  },
+};
